3b1916d7857008e195c1f5a5640be961d82e6975
arthur.yoon <47406496+arthur-yoon@users.noreply.github.com>
2025-02-17 22:30:26 +0900

    Time: 0 ms (100%), Space: 17.9 MB (26.45%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,33 +1,15 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        # 정답문자열
-        result = ''
+        # 1. 공약수 문자열 존재 조건 확인
+        if str1 + str2 != str2 + str1:
+            return ""
 
-        # 각 문자열 길이
-        n1 = len(str1)
-        n2 = len(str2)
+        # 2. GCD 알고리즘의 문자열 적용 (반복적 축소)
+        while str2: # str2가 비어있지 않은 동안 반복 (while str2 != "") 와 동일
+            if len(str1) < len(str2):
+                str1, str2 = str2, str1 # 파이썬 스타일의 swap
 
-        # 더 짧은 문자열 길이
-        shorter_len = min(n1, n2)  
+            str1 = str1[len(str2):] # str1에서 str2 길이만큼 prefix 제거
 
-        # divides 함수
-        def divides(s: str, t: str) -> bool:
-
-            if len(s) % len(t) != 0:
-                return False
-            repeat_count = len(s) // len(t)
-            constructed_s = ""
-            for _ in range(repeat_count):
-                constructed_s += t
-            return constructed_s == s
-
-        # 가능한 접두사 길이 (shorter_len 부터 1까지 내림차순) 만큼 반복
-        for length in range(shorter_len, 0, -1):
-            potential_divisor = str1[:length] # 접두사 후보 (str1에서 추출)
-
-            # potential_divisor 가 str1과 str2를 모두 나누는지 확인
-            if divides(str1, potential_divisor) and divides(str2, potential_divisor):
-                return potential_divisor # 공약수 문자열 발견 (가장 긴 것부터 찾았으므로 최대 공약수)
-        
-        # 공약수 문자열을 찾지 못한 경우, 빈 문자열 반환
-        return result 
+        # 3. 기저 조건: str2가 빈 문자열이 되면 str1 반환
+        return str1
