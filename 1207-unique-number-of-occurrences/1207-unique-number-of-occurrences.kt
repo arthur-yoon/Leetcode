24bd931b2f003f9d0b367ba1a8678e2ef7b1821b
arthur.yoon <47406496+arthur-yoon@users.noreply.github.com>
2025-02-19 21:20:38 +0900

    Time: 25 ms (16.25%), Space: 39.3 MB (5.83%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.kt
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.kt
@@ -1,18 +1,27 @@
+// class Solution {
+//     fun uniqueOccurrences(arr: IntArray): Boolean {
+//         val valueOccurrencesMap = HashMap<Int, Int>()
+
+//         for (num in arr) {
+//             valueOccurrencesMap[num] = (valueOccurrencesMap.get(num) ?: 0) + 1
+//         }
+
+//         val valueOccurrencesSet = HashSet<Int>()
+//         for (occurences in valueOccurrencesMap.values) {
+//             if (valueOccurrencesSet.contains(occurences)) {
+//                 return false
+//             }
+//             valueOccurrencesSet.add(occurences)
+//         }
+//         return true
+//     }
+// }
+
 class Solution {
     fun uniqueOccurrences(arr: IntArray): Boolean {
-        val valueOccurrencesMap = HashMap<Int, Int>()
+        val frequencyMap = arr.toList().groupingBy { it }.eachCount()
 
-        for (num in arr) {
-            valueOccurrencesMap[num] = (valueOccurrencesMap.get(num) ?: 0) + 1
-        }
-
-        val valueOccurrencesSet = HashSet<Int>()
-        for (occurences in valueOccurrencesMap.values) {
-            if (valueOccurrencesSet.contains(occurences)) {
-                return false
-            }
-            valueOccurrencesSet.add(occurences)
-        }
-        return true
+        val frequencySet = HashSet<Int>(frequencyMap.values)
+        return frequencySet.size == frequencyMap.size
     }
 }
