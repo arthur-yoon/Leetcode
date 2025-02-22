aee01125c7aa0791920c86da79735ba995db3913
arthur.yoon <47406496+arthur-yoon@users.noreply.github.com>
2025-02-22 18:29:21 +0900

    Time: 62 ms (70%), Space: 46.6 MB (70%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.kt
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.kt
@@ -1,49 +1,15 @@
-import java.util.PriorityQueue
-
 class SmallestInfiniteSet() {
-    private val minHeap: PriorityQueue<Int> = PriorityQueue()
-    private var currentSmallestPositiveInteger = 1
+    private var cur = 1
+    private val minHeap = PriorityQueue<Int>() // 최소 힙 PriorityQueue
 
     fun popSmallest(): Int {
-        println("popSmallest() 호출 시작 - 현재 상태: minHeap=$minHeap, currentSmallestPositiveInteger=$currentSmallestPositiveInteger") // 로그 시작
-
-        if (minHeap.isNotEmpty()) {
-            val smallestFromHeap = minHeap.peek()
-            println("minHeapNotEmpty: smallestFromHeap=$smallestFromHeap") // 로그: 힙이 비어있지 않음
-            if (smallestFromHeap <= currentSmallestPositiveInteger) {
-                val poppedValue = minHeap.poll()
-                println("힙에서 pop: $poppedValue") // 로그: 힙에서 pop 발생
-                println("popSmallest() 반환: $poppedValue,  상태 변경: minHeap=$minHeap, currentSmallestPositiveInteger=$currentSmallestPositiveInteger") // 로그 반환 및 상태
-                return poppedValue
-            } else {
-                println("힙 최소값(${smallestFromHeap}) > currentSmallestPositiveInteger(${currentSmallestPositiveInteger}), currentSmallestPositiveInteger 반환") // 로그: 힙 최소값 > currentSmallestPositiveInteger
-            }
-        } else {
-            println("minHeapEmpty: 힙이 비어있음") // 로그: 힙이 비어있음
-        }
-
-        val smallest = currentSmallestPositiveInteger
-        currentSmallestPositiveInteger++
-        println("기본 값 pop: $smallest, 상태 변경: minHeap=$minHeap, currentSmallestPositiveInteger=$currentSmallestPositiveInteger") // 로그: 기본 값 pop
-        println("popSmallest() 호출 종료 - 반환 값: $smallest") // 로그 종료
-        return smallest
+        return if (minHeap.isEmpty()) cur++ // 힙이 비어있으면 cur 반환 후 증가
+        else minHeap.poll() // 힙이 비어있지 않으면 힙에서 poll
     }
 
     fun addBack(num: Int) {
-        println("addBack($num) 호출 시작 - 현재 상태: minHeap=$minHeap, currentSmallestPositiveInteger=$currentSmallestPositiveInteger") // 로그 시작
-        if (num < currentSmallestPositiveInteger) {
-            minHeap.offer(num)
-            println("minHeap에 $num 추가 - 상태 변경: minHeap=$minHeap, currentSmallestPositiveInteger=$currentSmallestPositiveInteger") // 로그: 힙에 추가
-        } else {
-            println("addBack($num) 조건 불만족 (num >= currentSmallestPositiveInteger), minHeap 변경 없음") // 로그: 조건 불만족
+        if (num < cur && !minHeap.contains(num)) { // 중복 체크 추가
+            minHeap.add(num)
         }
-        println("addBack($num) 호출 종료 - 상태: minHeap=$minHeap, currentSmallestPositiveInteger=$currentSmallestPositiveInteger") // 로그 종료
     }
 }
-
-/**
- * Your SmallestInfiniteSet object will be instantiated and called as such:
- * var obj = SmallestInfiniteSet()
- * var param_1 = obj.popSmallest()
- * obj.addBack(num)
- */
