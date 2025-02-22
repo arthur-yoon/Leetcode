62aad38145e91c3ce8811f1e97073e8b034330a4
arthur.yoon <47406496+arthur-yoon@users.noreply.github.com>
2025-02-22 18:29:30 +0900

    Time: 60 ms (76.25%), Space: 45.5 MB (78.75%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.kt
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.kt
@@ -1,14 +1,14 @@
 class SmallestInfiniteSet() {
-    private var cur = 1
+    private var currentMin = 1
     private val minHeap = PriorityQueue<Int>() // 최소 힙 PriorityQueue
 
     fun popSmallest(): Int {
-        return if (minHeap.isEmpty()) cur++ // 힙이 비어있으면 cur 반환 후 증가
+        return if (minHeap.isEmpty()) currentMin++ // 힙이 비어있으면 currentMin 반환 후 증가
         else minHeap.poll() // 힙이 비어있지 않으면 힙에서 poll
     }
 
     fun addBack(num: Int) {
-        if (num < cur && !minHeap.contains(num)) { // 중복 체크 추가
+        if (num < currentMin && !minHeap.contains(num)) { // 중복 체크 추가
             minHeap.add(num)
         }
     }
