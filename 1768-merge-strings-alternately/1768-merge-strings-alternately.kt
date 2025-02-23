8a47c838c81b8f2c7c550d518f2e9869f551604e
arthur.yoon <47406496+arthur-yoon@users.noreply.github.com>
2025-02-24 02:14:28 +0900

    Time: 152 ms (55.2%), Space: 35.5 MB (58.94%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.kt
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.kt
@@ -1,6 +1,6 @@
 class Solution {
     fun mergeAlternately(word1: String, word2: String): String {
-        // 1. 결과 문자열을 저장할 merged_string (혹은 StringBuilder) 을 초기화한다.
+        // 1. 결과 문자열을 저장할 merged_string
         val mergedString = StringBuilder()
         
         // 2. word1과 word2의 현재 인덱스를 나타내는 변수 i와 j를 0으로 초기화한다.
@@ -9,24 +9,20 @@ class Solution {
 
         // 3. i가 word1의 길이보다 작고 j가 word2의 길이보다 작은 동안 반복한다.
         while (i < word1.length && j < word2.length) {
-            // word1[i]를 merged_string에 추가한다.
-            mergedString.append(word1[i])
-            // word2[j]를 merged_string에 추가한다.
-            mergedString.append(word2[j])
+            mergedString.append(word1[i]) // word1[i]를 merged_string에 추가한다.
+            mergedString.append(word2[j]) // word2[j]를 merged_string에 추가한다.
             // i와 j를 1씩 증가시킨다.
             i++
             j++
         }
 
-        // 4. 만약 i가 word1의 길이보다 작다면 (word1에 남은 문자가 있다면),
-        //    word1[i]부터 word1의 끝까지 merged_string에 추가한다.
+        // 4. 만약 i가 word1의 길이보다 작다면 (word1에 남은 문자가 있다면), word1[i]부터 word1의 끝까지 merged_string에 추가한다.
         while (i < word1.length) {
             mergedString.append(word1[i])
             i++
         }
 
-        // 5. 만약 j가 word2의 길이보다 작다면 (word2에 남은 문자가 있다면),
-        //    word2[j]부터 word2의 끝까지 merged_string에 추가한다.
+        // 5. 만약 j가 word2의 길이보다 작다면 (word2에 남은 문자가 있다면), word2[j]부터 word2의 끝까지 merged_string에 추가한다.
         while (j < word2.length) {
             mergedString.append(word2[j])
             j++
