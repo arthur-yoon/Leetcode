7e8d0a3b60da551669fd478e378461bd236c68f8
arthur.yoon <47406496+arthur-yoon@users.noreply.github.com>
2025-02-23 17:19:22 +0900

    Time: 6 ms (83.12%), Space: 38.4 MB (73.84%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.kt
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.kt
@@ -1,24 +1,31 @@
 class Solution {
-    private val letterOfDigit = mapOf(
-        '2' to "abc".toList(),
-        '3' to "def".toList(),
-        '4' to "ghi".toList(),
-        '5' to "jkl".toList(),
-        '6' to "mno".toList(),
-        '7' to "pqrs".toList(),
-        '8' to "tuv".toList(),
-        '9' to "wxyz".toList(),
-    )
-
-    private fun List<String>.combine(letters: List<Char>): List<String> =
-        letters.flatMap { c -> this.map { it + c } }
-
     fun letterCombinations(digits: String): List<String> {
         if (digits.isEmpty()) return emptyList()
-        var res = listOf<String>("")
-        for (digit in digits) {
-           res = res.combine(letters = letterOfDigit.getOrDefault(digit, listOf()))
+        val result = mutableListOf<String>()
+        val digitsLength = digits.length
+        fun dfs(i: Int, current: String) {
+            if (i >= digitsLength) {
+                result.add(current)
+                return
+            }
+
+            for (letter in map.getValue(digits[i])) {
+                dfs(i + 1, current + letter)
+            }
         }
-        return res
+
+        dfs(0, "")
+        return result
     }
 }
+
+val map = mapOf(
+        '2' to "abc",
+        '3' to "def",
+        '4' to "ghi",
+        '5' to "jkl",
+        '6' to "mno",
+        '7' to "pqrs",
+        '8' to "tuv",
+        '9' to "wxyz",
+)
